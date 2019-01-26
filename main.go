7c4b0eb58d08141cee8c0cdde60fb41cfff957b8
Adron Hall <adron@thrashingcode.com>
2019-01-26 11:55:47 -0800

    Created plus refactored the original code to print out via a collected string variable that we can also export to file.

--- main.go
+++ main.go
@@ -6,36 +6,59 @@ import (
 	"strconv"
 )
 
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
 func main() {
-	var someInt int
-	var anotherValue int64
-	thisValue := 45
-	thisVAlue, thatValue, someValue := 20, 23, 15
-	someInt = thisVAlue
-	anotherValue = 64
 
-	const knownSet = 7.12904
-	const anotherKnown = 9.10347
+	var outputResult string
 
-	for x := 0; x < 20; x++ {
-		fmt.Printf("x = %d  e = %8.3f\n", x, math.Exp(float64(x)))
+	outputResult = "<svg xmlns='http://www.w3.org/2000/svg' " +
+		"style='stroke: grey; fill: white; stroke-width: 0.7' " +
+		"width='" + strconv.Itoa(width) + "' height='" + strconv.Itoa(height) + "'>"
+
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+
+			outputResult = outputResult + "<polygon points='" +
+				ax + "," + ay +
+				bx + "," + by +
+				cx + "," + cy +
+				dx + "," + dy +
+				"/>\n"
+		}
 	}
-
-	fmt.Printf("Known Set and Another Known: %2.7f, %2.7f", knownSet, anotherKnown)
-
-	fmt.Printf("The values: %6d, %6d, %6d, and %s.\n", thisValue, thisVAlue, thatValue, strconv.Itoa(someValue))
-	fmt.Printf("Other values: %4d, %4d.\n", someInt, anotherValue)
-
-	fmt.Printf("Function doThings: %6d\n", doThings(thisVAlue, thisValue))
-	fmt.Printf("Function doMoreThings: %s", doMoreThings(someInt, int(anotherValue), "Adron"))
+	outputResult = outputResult + "</svg>"
+	fmt.Printf(outputResult)
 }
 
-func doThings (valueOne int, valueTwo int) int64 {
-	result := valueOne * valueTwo
-	return int64(result)
+func corner(i, j int) (string, string) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+
+	sx := width/2 + (x+y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	xResult := strconv.FormatFloat(sx, 'f', -1, 64)
+	yResult := strconv.FormatFloat(sy, 'f', -1, 64)
+	return xResult, yResult
 }
 
-func doMoreThings (valueOne int, valueTwo int, name string) string {
-	result := valueTwo + valueOne * 10
-	return "The result added and exponentially multiplied for you " + name + " this: " + strconv.Itoa(result)
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r)
 }
